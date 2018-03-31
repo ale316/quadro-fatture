b0a6b0cb91087f252589fe3e0001f9385b86de0e
Alessandro Marin <alessandro.m316@gmail.com>
2018-03-30 21:41:13 -0400

    make sure this doesn't crash

--- server/jobs/fetchDocuments.js
+++ server/jobs/fetchDocuments.js
@@ -2,6 +2,7 @@ const Documents = require('../repositories/Documents')
 const MongoClient = require('mongodb').MongoClient
 
 let format = function(invoice) {
+    console.log(invoice)
     return {
         _id: invoice.id,
         token: invoice.token,
@@ -12,31 +13,35 @@ let format = function(invoice) {
         type: invoice.tipo,
         paid: invoice.tipo === "fatture", // if it's "fatture" it's been paid
         link_doc: invoice.link_doc,
-        items: invoice.lista_articoli ? invoice.lista_articoli.map(a => { return { _id: a.id, description: a.descrizione } }) : []
+        items: invoice.lista_articoli && invoice.lista_articoli.length > 0 ? invoice.lista_articoli.map(a => {
+            return { _id: a.id, code: a.codice, description: a.descrizione } 
+        }) : []
     }
 }
 
 module.exports = async function() {
-    const url = 'mongodb://localhost:27017'
-    let client = await MongoClient.connect(url)
-    let db = client.db('fatture')
+    try {
+        const url = 'mongodb://localhost:27017'
+        let client = await MongoClient.connect(url)
+        let db = client.db('fatture')
 
-    let years = [2016, 2017, 2018]
-    let paid = []
-    let unpaid = []
-    for (var i = 0; i < years.length; i++) {
-        let year = years[i]
-        let currPaid = await Documents.getAll('fatture', year)
-        paid = paid.concat(currPaid)
+        let years = [2016, 2017, 2018]
+        let paid = []
+        let unpaid = []
+        for (var i = 0; i < years.length; i++) {
+            let year = years[i]
+            let currPaid = await Documents.getAll('fatture', year)
+            paid = paid.concat(currPaid)
 
-        let currUnpaid = await Documents.getAll('proforma', year)
-        unpaid = unpaid.concat(currUnpaid)
+            let currUnpaid = await Documents.getAll('proforma', year)
+            unpaid = unpaid.concat(currUnpaid)
+        }
+
+        unpaid = await Documents.getDetails(unpaid)
+
+        let all = paid.concat(unpaid).map(i => format(i))
+        await db.collection('invoices').update(all, { upsert: true, multi: true })
+    } catch (e) {
+        console.log(e)
     }
-
-    unpaid = await Documents.getDetails(unpaid)
-    console.log(unpaid)
-
-    let all = paid.concat(unpaid).map(i => format(i))
-
-    // await db.collection('invoices').update(all, { upsert: true, multi: true })
 }()
